b45bd2be3d0d91e3b196b7ae1dabf8797893da9c
Joshua Siddle <106534417+Joshsiddle@users.noreply.github.com>
2022-08-03 17:14:21 +0100

    added books page

--- library/src/App.js
+++ library/src/App.js
@@ -1,10 +1,21 @@
+import './index.css'
 import Nav from './components/nav'
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from './data';
 
 function App() {
   return (
-    <div >
-      <Nav />
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={Books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
